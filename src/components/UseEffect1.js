636306a4848c666ee3ba552f9add6336de3b7cc0
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-05 18:45:22 +0000

    updated with useEffect conditional rendering

--- src/components/UseEffect1.js
+++ src/components/UseEffect1.js
@@ -2,9 +2,9 @@
 import React, {useState,useEffect} from 'react'
 
 function EffectCount (){
-
-useEffect(()=>{document.title=`Push ${count} times`})
- const [count,Incount]=useState(0)
+    const [count,Incount]=useState(0)
+useEffect(()=>{document.title=`Push ${count} times`},[count])
+ 
     return(
         <div>
             <button onClick={()=>Incount(prevcount=>prevcount+1)}>Count</button>
