1afde528cc0c5985b089152a8122dbf4eec6e224
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-06 10:25:38 +0000

    updated learning on useFFect hook,getting mouse coordinate and setting the component to render only once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import HookCounter from "./components/HookCounter";
 import HookCountertwo from "./components/HookCounter2";
 import UseStateWithObject from "./components/UseStateObject";
-
 import EffectCount from "./components/UseEffect1";
+import MouseCoordinate from "./components/MouseHook";
 function App() {
   return (
     <div className="App">
-    <EffectCount/>
+    <MouseCoordinate/>
+    {/* <EffectCount/> */}
 {/* <UseStateWithObject/> */}
     {/* <HookCountertwo/> */}
 {/*      <HookCounter/> */}
---
+++ src/components/MouseHook.js
@@ -0,0 +1,22 @@
+/* working with useeffect to get the mouse coordinate on the browser window
+ */
+import React,{useEffect,useState} from "react"
+
+function MouseCoordinate (){
+    const[x,setX]=useState(0)
+    const[y,setY]=useState(0)
+
+    const getMousePoint=(e)=>{
+        console.log("mouseOVer")
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+    useEffect(()=>{
+        window.addEventListener('mousemove',getMousePoint)
+    },[])
+
+    return(
+        <div>X:{x} Y:{y}</div>
+    )
+}
+export default MouseCoordinate;
