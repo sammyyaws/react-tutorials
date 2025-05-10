9d9db20bce167862d43b6433dcf54bdc74fa1371
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-10 18:14:23 +0000

    Update:styling the active link base on the isActive property of the  NavLink component

--- src/comonents/Navbar.jsx
+++ src/comonents/Navbar.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './style.css'
 export function Navbar() {
+  const setStyle=({isActive})=>{
+   return({
+     color: isActive? "aliceblue": "black"
+   })
+  }
   return (
     <>
     <nav>
-        <NavLink to="/">Home</NavLink>
-         <NavLink to="/Products">Products</NavLink>
+        <NavLink style={setStyle} to="/">Home</NavLink>
+         <NavLink style={setStyle} to="/Products">Products</NavLink>
     </nav>
       
     </>
