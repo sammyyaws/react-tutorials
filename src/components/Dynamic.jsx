5a5ae88757d264111c01627888103cfda592ebea
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-12 18:11:44 +0000

    Update:started learning on usesearchParams in react router

--- src/components/Dynamic.jsx
+++ src/components/Dynamic.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { useParams,useSearchParams } from 'react-router-dom'
 function Dynamic() {
+     const {ItemId}=useParams()
+   const [searchParam,setSearchParam]=useSearchParams()
+   const showActive=searchParam.get("filter")==="active"
   return (
     <div>
       This is a dynamic page
+      <button onClick={()=>{setSearchParam({filter:"active"})}}>Filter</button>
+       <button onClick={()=>{setSearchParam({})}}>reset</button>
+       {showActive? <h1>We are active</h1>:<h4>We are not active</h4>}
     </div>
   )
 }
