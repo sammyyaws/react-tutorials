d75344ad318fd31ff6d3ffb0f3ce4658aad0c281
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-12 11:44:49 +0000

    Update:updated on how to render if there is no match of the path being searched

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Products from "./comonents/Products"
 import {Routes,Route} from 'react-router-dom'
 import {Navbar} from "./comonents/Navbar"
 import Result from "./comonents/Result"
+import Nomatch from "./comonents/Nomatch"
 function App() {
   
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
         <Route path="/Products" element={<Products/>}/>
         <Route path="/results" element={<Result/>}/>
+        <Route path="*" element={<Nomatch/>}/>
      </Routes>
      
     </div>
---
+++ src/comonents/Nomatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nomatch() {
+  return (
+    <div>
+    <h1>Error 419</h1>
+    This page does not exist
+    </div>
+  )
+}
+
+export default Nomatch
