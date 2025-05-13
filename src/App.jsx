6af4d3f01ed93b98d2a60f0fc1758665326ce83e
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-13 18:31:11 +0000

    Update:installed tailwindcss and used it to style the form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import InstaForms from "./components/InstaForms"
+import "./styles/styles.css"
 
 function App() {
  
 
   return (
-    <>
-   
-    </>
+    <div className="items-center justify-center flex h-screen w-screen  bg-opacity-90">
+  
+   <InstaForms/>
+    </div>
   )
 }
 
--- src/components/InstaForms.jsx
+++ src/components/InstaForms.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import "../styles/styles.css"
 function InstaForms() {
   return (
-    <div>
+   
+      <div className=' flex  justify-center items-center backdrop-blur-md  bg-white shadow-md md:w-1/2 h-80 w-60 lg:w-1/4 lg:h-1/2 rounded-md bg-'>
+      <form >
+    
+       <label className=' m-2 block' >
+            Firstname </label>
+              <input className="border-1  block outline-none rounded-sm py-1 px-2  h-full w-full"type="text" name="Firstname" id="Firstname"/>    
+   
+
+          <label className=' m-2 block'>Email  </label>
+         <input className="border-1   block outline-none  rounded-sm py-1 px-2  h-full w-full" type="text" name="email" id="email"/>
       
-    </div>
+     
+   
+
+          <label className=' m-2 block'>Username  </label>
+
+          <input className="border-1   block outline-none  rounded-sm py-1 px-2 h-full w-full" type="text" name="Username" id="Username"/>
+    <button className='bg-blue-300 block my-3 rounded-md py-1 px-2'>Submit</button>
+      </form>
+         </div>
+   
   )
 }
 
