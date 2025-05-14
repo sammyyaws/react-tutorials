4bead5778885e5f261c485a20dc1ec428a2ff4b5
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-14 11:47:11 +0000

    Update:working on form field state management with formik

--- src/components/InstaForms.jsx
+++ src/components/InstaForms.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 import "../styles/styles.css"
+import {formbox,label,input,button}from '../styles/FormClass'
+import { useFormik } from 'formik'
+
 function InstaForms() {
+const formik=useFormik({
+  initialValues:{
+    Firstname:'',
+    email:'',
+    Username:''
+  }
+})
+
+
+
+
+
+
+
+
   return (
    
-      <div className=' flex  justify-center items-center backdrop-blur-md  bg-white shadow-md md:w-1/2 h-80 w-60 lg:w-1/4 lg:h-1/2 rounded-md bg-'>
+      <div className={formbox}>
       <form >
     
-       <label className=' m-2 block' >
+       <label className={label} >
             Firstname </label>
-              <input className="border-1  block outline-none rounded-sm py-1 px-2  h-full w-full"type="text" name="Firstname" id="Firstname"/>    
+              <input className={input} type="text" name="Firstname" id="Firstname" onChange={formik.handleChange} value={formik.values.Firstname}/>    
    
 
-          <label className=' m-2 block'>Email  </label>
-         <input className="border-1   block outline-none  rounded-sm py-1 px-2  h-full w-full" type="text" name="email" id="email"/>
+          <label className={label}>Email  </label>
+            <input className={input} type="text" name="email" id="email" onChange={formik.handleChange} value={formik.values.email} />
       
      
    
 
-          <label className=' m-2 block'>Username  </label>
+          <label className={label}>Username  </label>
 
-          <input className="border-1   block outline-none  rounded-sm py-1 px-2 h-full w-full" type="text" name="Username" id="Username"/>
-    <button className='bg-blue-300 block my-3 rounded-md py-1 px-2'>Submit</button>
+          <input className={input} type="text" name="Username" id="Username" onChange={formik.handleChange} value={formik.values.Username}/>
+    <button className={button}>Submit</button>
       </form>
          </div>
    
---
+++ src/styles/FormClass.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { clsx } from 'clsx'
+
+export const formbox=clsx(
+     'flex' ,
+      'justify-center',
+      'items-center',
+      'backdrop-blur-md',
+       'bg-white shadow-md',
+        'md:w-1/2', 
+        'h-80',
+         'w-60',
+          'lg:w-1/4',
+           'lg:h-1/2',
+            'rounded-md bg'
+)
+
+export const label=clsx(
+    ' m-2',
+    ' block'
+)
+export const input=clsx(
+    "border-1", 
+    'block',
+     'outline-none', 
+     'rounded-sm', 
+     'py-1', 
+     'px-2',
+       'h-full', 
+       'w-full',
+)
+
+export const button=clsx(
+    'bg-blue-300',
+     'block',
+      'my-3',
+       'rounded-md',
+        'py-1',
+         'px-2',
+)
