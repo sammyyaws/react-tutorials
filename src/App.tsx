c0adf2f05b267cf30a94b966858c02bcc0f09131
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-22 17:39:02 +0000

    started working on react typescript: typing props passed to components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/style.css"
+import Greet from "./components/Greet"
+function App() {
+
+
+  return (
+    < >
+      <Greet name='Samuel' messages={10} isLoggedin={true}/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greet.tsx
@@ -0,0 +1,16 @@
+type Props={
+    name:string;
+    messages:number;
+    isLoggedin:boolean
+}
+
+function Greet(props:Props) {
+    const {name, messages, isLoggedin}=props
+  return (
+    <div>
+     { isLoggedin ? <h1> Hi {name} you have {messages} </h1>:<h3>Please you are logged out</h3>}
+    </div>
+  )
+}
+
+export default Greet
