fb942b2846c0834b80da071cb124c67b27b3fea8
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-27 08:58:45 +0000

    Update:Learnt on the instance of having the an unknown state that can change in the future we have to explicity state the possible type that is going to be assigned to the state

--- src/components/Export.types.ts
+++ src/components/Export.types.ts
@@ -1,3 +1,8 @@
 export type ContainerProps={
     style:React.CSSProperties;
 }
+
+export type userDataType={
+    name:string;
+    email:string;
+}
--- src/components/StatesTyping/User.tsx
+++ src/components/StatesTyping/User.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react"
+import type { userDataType } from "../Export.types"
+
+const userData={
+    name:"Gomerand",
+    email:"Gigi@gmail.com"
+}
 
 function User() {
-const [LogState,setLogState]=useState(false)
+const [LogState,setLogState]=useState<userDataType|null>(null)
 const handleLogin=()=>{
-setLogState(true)
+setLogState(userData)
 }
 const handleLogout=()=>{
-setLogState(false)
+setLogState(null)
 }
 
   return (
     <div>
       <button className="px-3 m-1.5 rounded-sm bg-amber-500" onClick={handleLogout}> LOG OUT</button>
       <button className="px-3 m-1.5 rounded-md bg-amber-500"  onClick={handleLogin}> LOG IN</button>
-<div>{LogState?<h3>You have successfully logged in</h3>:<h3>You have logged out</h3>}</div>
+<div>{LogState?<h3>You have successfully logged in as {LogState.name}</h3>:<h3>You have logged out</h3>}</div>
     </div>
   )
 }
