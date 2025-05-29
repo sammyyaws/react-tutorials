9711a27b388f3b5d99e7930d2c69d744ad982828
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2025-05-29 11:29:22 +0000

    Typing of useRef hook

---
+++ src/components/StatesTyping/UseRef.tsx
@@ -0,0 +1,16 @@
+import { useRef,useEffect } from "react"
+
+function UseRef() {
+    const inputRef=useRef<HTMLInputElement>(null);
+
+    useEffect(()=>{
+        inputRef.current?.focus()
+    },[])
+  return (
+    <div>
+      <input ref={inputRef}/>
+    </div>
+  )
+}
+
+export default UseRef
