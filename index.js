82f02d353e18ba7bfdd4e28009a76f8eb6177f25
Engineer Sam <sammyyawsnana1@gmail.com>
2025-06-17 18:19:50 +0000

    Update: learnt how to create  reducer function for  editing the state

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const VideoAdded="VideoAdded";
 
+
+//action creator
+
 function GetVideo(){
 {
     type=VideoAdded;
 }}
+//state declaration which is supposed to be an object always
+
+const initialState={
+    video:10,
+}
+
+//reducer 
+
+function reducer(state=initialState,action){
+
+    switch(action){
+     case VideoAdded:
+        return state.video+1;
+     default:
+        return state;
+    }
+
+
+}
