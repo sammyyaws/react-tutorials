f635b2e6c23316acc843d8f069f960792c48c881
Engineer Sam <sammyyawsnana1@gmail.com>
2026-02-02 07:55:02 +0000

    finished learning about the slice concept,store configuration in redux toolkit

---
+++ index.js
@@ -0,0 +1,16 @@
+const store=require("./app/store")
+const cakeActions=require("./app/features/cake/cakeSlice").cakeActions
+
+
+console.log("initial state:",store.getState())
+const    unsubscribe=store.subscribe(()=>{
+    console.log("updated state:",store.getState())
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+
+store.dispatch(cakeActions.restock(5))
+
+unsubscribe()
